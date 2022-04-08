6e775465a3552469468a986655aa8dca5415dfdf
G-Ilian <gbiel.boza@gmail.com>
2022-04-08 16:45:41 -0300

    Primeira versão do BD, com conexão para o MongoDB

---
+++ Ex Avaliativo - Simula Sensores/db/bancodedados.py
@@ -0,0 +1,18 @@
+import pymongo
+
+class Database:
+    def __init__(self,database,collection):
+        connectionString = "mongodb+srv://admin:admin@bancoiot.df2gm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        self.clusterConnection = pymongo.MongoClient(
+            connectionString,
+            # CASO OCORRA O ERRO [SSL_INVALID_CERTIFICATE]
+            tlsAllowInvalidCertificates=True
+        )
+        self.db = self.clusterConnection[database]
+        self.collection = self.db[collection]
+
+    def create(self, nome_sensor,valor_sensor,sensor_alarmado):
+        return self.collection.insert_one({"Sensor": nome_sensor,"Temperatura":valor_sensor,"Sensor Alarmado":sensor_alarmado})
+    
+    def resetDatabase(self):
+        self.db.drop_collection(self.collection)
