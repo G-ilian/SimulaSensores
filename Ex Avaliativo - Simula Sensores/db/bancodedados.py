b3e04f8f5c401b1e646ed9eb1149d48c9fba90ca
G-Ilian <gbiel.boza@gmail.com>
2022-04-08 17:27:21 -0300

    Indicação da posição da url do banco

--- Ex Avaliativo - Simula Sensores/db/bancodedados.py
+++ Ex Avaliativo - Simula Sensores/db/bancodedados.py
@@ -2,7 +2,7 @@ import pymongo
 
 class Database:
     def __init__(self,database,collection):
-        connectionString = "mongodb+srv://admin:admin@bancoiot.df2gm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        connectionString = "url_do_seu_banco"
         self.clusterConnection = pymongo.MongoClient(
             connectionString,
             # CASO OCORRA O ERRO [SSL_INVALID_CERTIFICATE]
